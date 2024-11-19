186483f65ecee2a36d05e0fb4bbc8430ec7c8a44
bramsom <brayansmartinez0607@gmail.com>
2024-11-19 10:44:41 -0500

    cambios actualizados de la vista shop

--- app/src/main/java/com/example/wooperland_enserio/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/wooperland_enserio/navigation/AppNavigation.kt
@@ -6,7 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 
-import com.example.wooperland_enserio.ShopSreen.ShopScreen
+
 import com.example.wooperland_enserio.screens.AchievementsScreen
 import com.example.wooperland_enserio.screens.AddChildScreen
 import com.example.wooperland_enserio.screens.AvatarScreen
@@ -17,6 +17,7 @@ import com.example.wooperland_enserio.screens.HomeScreen
 import com.example.wooperland_enserio.screens.LoginScreen
 import com.example.wooperland_enserio.screens.ProfileScreen
 import com.example.wooperland_enserio.screens.RegisterScreen
+import com.example.wooperland_enserio.screens.ShopScreen
 import com.example.wooperland_enserio.screens.TermsScreen
 
 @Composable
